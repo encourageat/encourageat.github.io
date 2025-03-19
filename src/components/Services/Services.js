611e9ba002d317fd994ece358fd182da588cc656
encourageat <encourageat@gmail.com>
2025-03-20 01:58:04 +0530

    revised..

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -13,7 +13,7 @@ const Services = () => {
         <p>
         <h2>Services..</h2>
         Offers freelance development, consulting and online training.<br/><br/> Training is  on Identity and Access Management- IAM with focus on Keycloak.
-        To know more on it click <a href="/mainsite/#/training/" target="_blank" rel="noopener noreferrer" style={{ color: 'blue', textDecoration: 'underline' }}> here.</a>
+        To know more on it click <a href="/#/training/" target="_blank" rel="noopener noreferrer" style={{ color: 'blue', textDecoration: 'underline' }}> here.</a>
 
         <br/><br/>Other services include foundational training on Java and Spring Boot.
         
